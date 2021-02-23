0d0d17fa495809663ce82e2fe322f151639077ca
Rizqi Maulana <sakukode@gmail.com>
2021-02-23 19:07:06 +0700

    membuat fitur register

--- myauth/urls.py
+++ myauth/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 # import semua class View untuk modul Category dan News
-from .views import CustomLoginView, HomeView
+from .views import CustomLoginView, HomeView, CustomLogoutView, RegisterView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('home/', HomeView.as_view(), name='home'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register')
 ]
--- myauth/views.py
+++ myauth/views.py
@@ -1,6 +1,10 @@
 from django.views import View
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.contrib.auth import login, authenticate
+
+from .forms import SignUpForm
 
 
 class CustomLoginView(LoginView):
@@ -13,3 +17,28 @@ class HomeView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'accounts/login.html'
+    next_page = 'login'
+
+
+class RegisterView(View):
+    template_name = 'accounts/register.html'
+
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, self.template_name, {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+        
+        return render(request, self.template_name, {'form': form})
